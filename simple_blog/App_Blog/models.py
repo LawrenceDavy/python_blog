4110c8b47b232c36a6ba74b44c4cc18445b5ba2d
Lawrence Davy <lawrencedavy13@gmail.com>
2021-03-09 20:23:16 +0000

    added likes model to blog

--- simple_blog/App_Blog/models.py
+++ simple_blog/App_Blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Blog(models.Model):
     author = models.ForeignKey(User, related_name='post_author', on_delete=models.CASCADE)
     blog_title = models.CharField(max_length=300, verbose_name='Put a Title')
@@ -14,8 +15,15 @@ class Blog(models.Model):
         return self.blog_title
     
 
+
 class Comment(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='blog_comment')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comment")
     comment = models.TextField()
     comment_date = models.DateTimeField(auto_now_add=False)
+
+
+
+class Likes(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="liked_blog")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked_user')
