2e49049e0acb42b17adc3f284c55f7b28f37099e
Lawrence Davy <lawrencedavy13@gmail.com>
2021-03-09 20:13:21 +0000

    created blog model

--- simple_blog/App_Blog/models.py
+++ simple_blog/App_Blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Blog(models.Model):
+    author = models.ForeignKey(User, related_name='post_author', on_delete=models.CASCADE)
+    blog_title = models.CharField(max_length=300, verbose_name='Put a Title')
+    slug = models.SlugField(max_length=300, unique=True)
+    blog_content = models.TextField(verbose_name="What is on your mind?")
+    blog_image = models.ImageField(upload_to='blog_images', verbose_name='Blog Image')
+    publish_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.blog_title
+    
