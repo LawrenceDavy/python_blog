09608a4e6dc863c36731d6b08815f002739b7894
Lawrence Davy <lawrencedavy13@gmail.com>
2021-03-10 13:00:36 +0000

    user account sign up funtionality created in signup html and app login views

--- simple_blog/App_Login/views.py
+++ simple_blog/App_Login/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def sign_up(request):
+    form = UserCreationForm()
+    registered = False
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            registered = True
+
+    dict = {'form':form, 'registerd':registered}
+    return render(request, 'App_Login/sign_up.html', context=dict)
