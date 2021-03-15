a91744e70b3a5529893c279066bde886820dc090
Lawrence Davy <lawrencedavy13@gmail.com>
2021-03-15 16:23:52 +0000

    created url path for profile updater

--- simple_blog/App_Login/views.py
+++ simple_blog/App_Login/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from App_Login.form import SignUpForm
+from App_Login.form import SignUpForm, UserProfileChange
 
 def sign_up(request):
     form = SignUpForm()
@@ -46,3 +46,15 @@ def logout_user(request):
 @login_required
 def profile(request):
     return render(request, 'App_Login/profile.html', context={})
+
+
+@login_required
+def user_change(request):
+    current_user = request.user
+    form = UserProfileChange(instance=current_user)
+    if request.method == 'POST':
+        form = UserProfileChange(request.POST, instance=current_user)
+        if form.is_valid():
+            form.save()
+            form = UserProfileChange(instance=current_user)
+    return render(request, 'App_Login/change_profile.html', context={'form':form})
