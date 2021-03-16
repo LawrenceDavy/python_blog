18d386f22d415f7454a0490f5468ce9648afe3af
Lawrence Davy <lawrencedavy13@gmail.com>
2021-03-16 13:56:30 +0000

    removed unnecssary fields

--- simple_blog/App_Login/models.py
+++ simple_blog/App_Login/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='user_profile', on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='profile_pics', height_field=None, width_field=None, max_length=None)
+    profile_pic = models.ImageField(upload_to='profile_pics')
