d5718d1f8ab03f44031908b07cbeb72ee7aad2e0
Lawrence Davy <lawrencedavy13@gmail.com>
2021-03-16 12:46:17 +0000

    added password changer url path

--- simple_blog/App_Login/urls.py
+++ simple_blog/App_Login/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('change-profile/', views.user_change, name='user_change'),
+    path('password/', views.password_change, name='password_change'),
 ]
