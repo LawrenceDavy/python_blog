5b50abea1dd97af119a3cd04623ff3302ae270da
Lawrence Davy <lawrencedavy13@gmail.com>
2021-03-16 13:57:04 +0000

    added url path for changing profile image

--- simple_blog/App_Login/urls.py
+++ simple_blog/App_Login/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('change-profile/', views.user_change, name='user_change'),
     path('password/', views.password_change, name='password_change'),
+    path('change-profile-image', views.add_pro_pro, name='add_pro_pic')
 ]
